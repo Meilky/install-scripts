ef580009bfd2591d797760339c4016ce41f50a3d
Meilky <mikaeljean2k3@gmail.com>
2023-09-13 23:21:28 -0400

    Update: Fix: Nerd-font script

--- nerd-font/script.sh
+++ nerd-font/script.sh
@@ -5,3 +5,7 @@ sudo apt install zip unzip -y
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraMono.zip
 
 unzip FiraMono.zip -d FiraMono
+
+sudo mkdir -p /usr/share/fonts/firamono/
+
+sudo cp FiraMono/FiraMonoNerdFont* /usr/share/fonts/firamono/
