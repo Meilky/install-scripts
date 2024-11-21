d78ca996a2fad3f68edce8f5fca09454ac746238
Meilky <mikaeljean2k3@gmail.com>
2024-11-21 15:04:23 -0500

    feat: install nightly nvim

--- neovim/script.sh
+++ neovim/script.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -e
+
 sudo apt install ripgrep git -y
 
 HERE="$(pwd)"
 
 cd /tmp
 
-wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+wget -O nvim.appimage https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
 chmod u+x nvim.appimage
 
 ./nvim.appimage --appimage-extract
@@ -17,4 +19,10 @@ cd $HERE
 
 mkdir -p "$HOME/.config/nvim"
 
-git clone https://github.com/Meilky/neovim-config.git "$HOME/.config/nvim"
+if [ -d "$HOME/.config/nvim" ]; then
+	cd $HOME/.config/nvim
+
+	git pull
+else
+	git clone https://github.com/Meilky/neovim-config.git "$HOME/.config/nvim"
+fi;
